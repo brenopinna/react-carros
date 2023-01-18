894bcd6f2c3bd59535092f5ed0095912309ba98b
brenopinna <brenopinna1008@gmail.com>
2023-01-17 22:34:26 -0300

    add material ui at venda

--- src/pages/Venda/Search/Range/index.jsx
+++ src/pages/Venda/Search/Range/index.jsx
@@ -1,51 +1,50 @@
 import React, {useEffect} from 'react';
-import { Thumb, Track, Trail } from './styles';
+import { Slider } from '@mui/material';
+
 
 const Range = ({setValue}) => {
    const [minValue, maxValue] = [20000, 70000];
+   const defaultValue = minValue + (maxValue - minValue)/2;
 
-   useEffect(() => setValue(minValue), []);
+   useEffect(() => setValue(defaultValue), []);
 
-   let isDrag = false;
-
-   const body = document.querySelector('body');
-   const track = document.querySelector('.track');
-   const trail = document.querySelector('.trail');
-
-   function handleMouseDown(){
-      isDrag = true;
-      body.style.cursor = 'pointer';
-      body.style.userSelect = 'none';
+   const handleChange = e => {
+      setValue(e.target.value);
    }
 
-   function handleMouseUp(){
-      isDrag = false;
-      body.style.cursor = 'auto';
-      body.style.userSelect = 'auto';
-   }
-
-   body.addEventListener('mousemove', e => {
-      if(isDrag){
-         const trackWidth = +getComputedStyle(track).width.replace('px','');
-         let mouseX = e.pageX - track.getBoundingClientRect().x;
-         if(mouseX < 0) mouseX = 0;
-         else if(mouseX >= trackWidth) mouseX = trackWidth;
-         trail.style.width = mouseX + 'px';
-
-         const percentual = (mouseX / trackWidth) * 100;
-         let newValue = (((maxValue - minValue)/100) * percentual + minValue).toFixed(2);
-         setValue(newValue);
-      }
-   })
-
-   body.addEventListener('mouseup', handleMouseUp);
+   const defaultColor = '#eb2d2d'
 
    return (
-      <Track className='track' >
-         <Trail className='trail'>
-            <Thumb onMouseDown={handleMouseDown} />
-         </Trail>
-      </Track>
+      <Slider
+         min={minValue}
+         max={maxValue}
+         defaultValue={defaultValue}
+         onChange={handleChange}
+         sx={{
+            height: 13,
+            '& .MuiSlider-track': {
+               backgroundColor: defaultColor,
+               border: 'none',
+               borderTopRightRadius: 0,
+               borderBottomRightRadius: 0,
+            },
+            '& .MuiSlider-rail': {
+               backgroundColor: defaultColor,
+            },
+            '& .MuiSlider-thumb': {
+               backgroundColor: defaultColor,
+               height: 20,
+               width: 20,
+               boxShadow: '0px 0px 7px rgb(0 0 0 / 40%)'
+            },
+            '& .MuiSlider-thumb:hover, & .MuiSlider-thumb.Mui-focusVisible': {
+               boxShadow: `0px 0px 0px 8px ${defaultColor}28`
+            },
+            '& .MuiSlider-thumb.Mui-active': {
+               boxShadow: `0px 0px 0px 14px ${defaultColor}28`
+            }
+         }}
+      />
    );
 }
 
