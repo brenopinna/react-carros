2f45ebe447a9ac3ca08ecd3af2f24022a181a85e
brenopinna <brenopinna1008@gmail.com>
2023-01-18 22:56:04 -0300

    update database

--- src/database.js
+++ src/database.js
@@ -5,49 +5,57 @@ export const database = [
       name: 'Quase "O Opala"',
       description: 'Podia ser o carro do Sam e Dean, mas não é.',
       price: 20000,
-      image: carImage
+      image: carImage,
+      id: 1
    },
    {
       name: 'Van do Scooby Doo',
       description:'Claramente não é uma van.',
       price: 50000,
-      image: carImage
+      image: carImage,
+      id: 2
    },
    {
       name: 'Barraca de Dogão',
       description: 'Já perdi a criatividade',
       price: 20000,
-      image: carImage
+      image: carImage,
+      id: 3
    },
    {
       name: 'Canetazulmóvel',
       description: 'Olha se você não me ama então não me compre',
       price: 60000,
-      image: carImage
+      image: carImage,
+      id: 4
    },
    {
       name: 'Impala do Marshall',
       description: 'SEM COMIDA NO IMPALA.',
       price: 60000,
-      image: carImage
+      image: carImage,
+      id: 5
    },
    {
       name: 'Freddie o Fusca',
       description: "Carro massa 1982",
       price: 60000,
-      image: carImage
+      image: carImage,
+      id: 6
    },
    {
       name: 'Linkinpala',
       description: "Um bem irado dos 2000",
       price: 50000,
-      image: carImage
+      image: carImage,
+      id: 7
    },
    {
       name: 'Carro do Sherek',
       description: "Maneirin anos 90",
       price: 30000,
-      image: carImage
+      image: carImage,
+      id: 8
    }
 ]
 
